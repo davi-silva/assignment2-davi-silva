bd03d3f14273658711892a00b45e78982465b67c
Davi Silva <davi.cunha.silva.dev@gmail.com>
2025-08-19 15:23:42 +0200

    chore: clean up

--- frontend/src/pages/Items.js
+++ frontend/src/pages/Items.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { FixedSizeList } from "react-window";
 import { Link } from "react-router-dom";
